fc06541d57e45960674e4fb95d9f16c370f839e9
Alain <hello@alainbarthelemy.com>
2014-12-02 18:16:48 +0100

    init

---
+++ js/GUI.js
@@ -0,0 +1,82 @@
+function GUI(){
+
+	this.states = {
+				scenarios:'scenarios', 
+				controls:'controls', 
+				medias:'medias', 
+				settings:'settings'
+	};
+	
+	// initialize the current state
+	this.changeState(this.states.controls);
+	
+	this.addEventListeners();
+	
+
+	
+
+	
+}
+
+GUI.prototype.addEventListeners = function(){
+	console.log($(".nav-btn"));
+/*	$(".nav-btn").each(function(element){
+			$(this).on("click",navclick(event));
+	});*/
+	$(document).on('click', '.nav-btn', function(event){
+			navclick(event);
+	});
+	var scope = this
+	function navclick(event){
+		switch(event.currentTarget.id){
+			case "nav-btn-scenarios" : 
+				console.log("ok");
+				scope.changeState(scope.states.scenarios);
+				break;
+			case "nav-btn-controls" : 
+				scope.changeState(scope.states.controls);
+				break;
+			case "nav-btn-medias" : 
+				scope.changeState(scope.states.medias);
+				break;
+			case "nav-btn-settings" : 
+				scope.changeState(scope.states.settings);
+				break;
+		}
+	}
+}
+
+GUI.prototype.changeState = function(newState){
+	
+	if(newState == this.currentState || !(newState in this.states))
+		return;
+	
+	$(".content-section").each(function(section,index){
+			$(".content-section").addClass("hidden");
+			$(".nav-btn").removeClass("active");
+	});
+	
+	switch(newState){
+		case this.states.scenarios :
+			$("#scenario-section").removeClass("hidden");
+			$("#nav-btn-scenarios").addClass("active");
+			break;
+		case this.states.controls :
+			$("#controls-section").removeClass("hidden");
+			$("#nav-btn-controls").addClass("active");
+			break;
+		case this.states.medias :
+			$("#medias-section").removeClass("hidden");
+			$("#nav-btn-medias").addClass("active");
+			break;
+		case this.states.settings :
+			$("#settings-section").removeClass("hidden");
+			$("#nav-btn-settings").addClass("active");
+			break;
+		default : console.log("unknown state"); break;
+		
+	}
+	
+	this.currentState = newState;
+	
+}
